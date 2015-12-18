8752abf2cc44906f183b60e660b740aff2731398
vsrgv <vsrgv@users.noreply.github.com>
2015-12-19 02:53:00 +0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
     setTimeout(function(){
         $('body').addClass('loaded');
     }, 3000);
- 
+    setTimeout(function(){
+        $('#loader-wrapper').remove();
+    },5000);
 });
